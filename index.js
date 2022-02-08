ffa703bce14a292cca9152c6fbb56e14f7f46b01
Dustin Smith <dustinsmith1983@gmail.com>
2022-02-08 03:25:41 -0800

    finished input sections for all 3 employee types

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                          //
+//                            Requires and Global variable declarations                                   //
+//                                                                                                        //
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
@@ -5,23 +13,134 @@ const inquirer = require('inquirer');
 const path = require('path');
 const fs = require('fs');
 
-
 const teamList = [];
-/* const userIds = []; */
-//Write your app here
+
+const creatHTML = require('./src/htmlTemplate.js');
 
 
-function finalize(){
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                          //
+//                                 Employee information entry section                                      //
+//                                                                                                        //
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// Function to collect Manager Info
+function enterManager(){
+    inquirer
+        .prompt([
+            {type: "input",
+            name: "managerName",
+            message: "Please enter the Manager's name: ",
+            },
+            {type: "input",
+            name: "managerId",
+            message: "Please enter the Manager's ID: ",
+            },
+            {type: "input",
+            name: "managerEmail",
+            message: "Please enter the Manager's Email: ",
+            },
+            {type: "input",
+            name: "managerOffNum",
+            message: "Please enter the Manager's office number: ",
+            },
+        ])
+        .then((response) => {
+            const manager = new Manager(
+                response.managerName,
+                response.managerId,
+                response.managerEmail,
+                response.managerOffNum
+                );
+
+            teamList.push(manager);
+            /* userIds.push(response.managerId); */
+
+            teamBuilder();
+        });
 
 };
 
+// Function to collect Engineer info
+function addEngineer() {
+    inquirer
+    .prompt([
+        {type: "input",
+        name: "engineerName",
+        message: "Please enter the Engineer's name: ",
+        },
+        {type: "input",
+        name: "engineerId",
+        message: "Please enter the Engineer's ID: ",
+        },
+        {type: "input",
+        name: "engineerEmail",
+        message: "Please enter the Engineer's Email: ",
+        },
+        {type: "input",
+        name: "engineerGitHub",
+        message: "Please enter the Enginner's GitHub username: ",
+        },
+    ])
+    .then((response) => {
+        const engineer = new Engineer(
+          response.engineerName,
+          response.engineerId,
+          response.engineerEmail,
+          response.engineerGitHub,
+        );
+        teamMembers.push(enginner);
+        teamBuilder();
+      });
+};
+
+// Function to collect Intern info
 function addIntern(){
-
+    inquirer
+    .prompt([
+        {type: "input",
+        name: "internName",
+        message: "Please enter the Intern's name: ",
+        },
+        {type: "input",
+        name: "internId",
+        message: "Please enter the Intern's ID: ",
+        },
+        {type: "input",
+        name: "internEmail",
+        message: "Please enter the Intern's Email: ",
+        },
+        {type: "input",
+        name: "internSchool",
+        message: "Please enter the name of the Intern's School: ",
+        },
+    ])
+    .then((response) => {
+        const intern = new Intern(
+          response.internName,
+          response.internId,
+          response.internEmail,
+          response.internSchool
+        );
+        teamList.push(intern);
+        teamBuilder();
+      });
 };
 
-function addEngineer() {}
 
-;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                          //
+//                                      Utility Function Section                                           //
+//                                                                                                        //
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function startApp(){
+    enterManager();
+};
+
 
 function teamBuilder() {
     inquirer
@@ -43,41 +162,15 @@ function teamBuilder() {
         });
 };
 
-function enterManager(){
-    inquirer
-        .prompt([
-            {type: "input",
-            name: "managerName",
-            message: "Please enter the Managers name: ",
-            },
-            {type: "input",
-            name: "managerId",
-            message: "Please enter the Managers ID: ",
-            },
-            {type: "input",
-            name: "managerEmail",
-            message: "Please enter the Managers Email: ",
-            },
-            {type: "input",
-            name: "managerOffNum",
-            message: "Please enter the Managers office number: ",
-            },
-        ])
-        .then((response) => {
-            const manager = new Manager(
-                response.managerName,
-                response.managerId,
-                response.managerEmail,
-                response.managerOffNum
-                );
 
-            teamList.push(manager);
-            /* userIds.push(response.managerId); */
-
-            teamBuilder();
-        });
+function finalize(){
 
 };
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                          //
+//                                           Start Application                                             //
+//                                                                                                        //
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-enterManager();
+startApp();
